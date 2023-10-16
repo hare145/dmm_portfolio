ee7d9d71f7affe95cacdfb13376ebd6f34d494bd
hare145 <130465393+hare145@users.noreply.github.com>
2023-10-16 12:55:10 +0000

    user_edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -45,6 +45,6 @@ class Public::UsersController < ApplicationController
     :accounting_reason, :other_soft, :other_reason,
     :salary_soft, :salary_reason, :working_hours_soft, :working_hours_reason,
     :communication_soft, :communication_reason, :customer_info_soft, :customer_info_reason,
-    :customer_deposit_soft, :customer_billing_soft, :customer_billing_reason)
+    :customer_deposit_soft, :customer_deposit_reason, :customer_billing_soft, :customer_billing_reason)
   end
 end
